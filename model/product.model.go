97c318aad343334a14d63e81f625c9c0b02f1d38
Farid <farid.abulias@gmail.com>
2017-04-04 13:34:02 -0300

    product

--- model/product.model.go
+++ model/product.model.go
@@ -1,1 +1,10 @@
 package model
+
+import "log"
+
+func Products() []Product {
+	var providers []Product
+	_, err = dbmap.Select(providers, "SELECT * FROM providers")
+	log.Println(providers)
+	return 0
+}
